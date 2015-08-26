5379d4e7f18e2033103c2172bbe5e0cc65972081
pxiaofei <993590362@qq.com>
2015-08-26 14:25:48 +0800

    first commit

---
+++ 启动图/启动图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  启动图
+//
+//  Created by andy on 15/8/10.
+//  Copyright (c) 2015年 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 启动图/启动图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  启动图
+//
+//  Created by andy on 15/8/10.
+//  Copyright (c) 2015年 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
